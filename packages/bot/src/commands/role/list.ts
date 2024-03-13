b0afeebf0dc4a15e3900f8283c9cd44116777a10
didinele <didinele.dev@gmail.com>
2024-03-13 21:15:08 +0200

    feat: roles list

---
+++ packages/bot/src/commands/role/list.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client';
+import type { APIApplicationCommandSubcommandOption, ChatInputCommandInteraction } from 'discord.js';
+import { singleton } from 'tsyringe';
+import type { Subcommand } from '../../struct/Command';
+
+@singleton()
+export default class implements Subcommand {
+	public readonly interactionOptions: Omit<APIApplicationCommandSubcommandOption, 'type'> = {
+		name: 'list',
+		description: 'Lists configuration for all roles',
+	};
+
+	public constructor(private readonly prisma: PrismaClient) {}
+
+	public async handle(interaction: ChatInputCommandInteraction<'cached'>) {
+		const rawRoles = await this.prisma.role.findMany({
+			where: {
+				guildId: interaction.guildId,
+			},
+		});
+
+		if (!rawRoles.length) {
+			await interaction.reply('No roles have been configured yet.');
+			return;
+		}
+
+		const roles = rawRoles.map((role) => `<@&${role.roleId}>`).filter(Boolean);
+
+		return interaction.reply({
+			content: roles.map((role) => `• ${role.toString()} (${role})`).join('\n'),
+			allowedMentions: { roles: [] },
+		});
+	}
+}
